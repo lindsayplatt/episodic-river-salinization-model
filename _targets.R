5fc4bd2cd284aea39334c390199dbf88a5cd504a
hdugan <hilarydugan@gmail.com>
2024-05-02 13:25:46 -0500

    turn off p5 and p7 for now

--- _targets.R
+++ _targets.R
@@ -39,12 +39,11 @@ source('1_Download.R')
 source('2_Prepare.R')
 source('3_Filter.R')
 source('4_EpisodicSalinization.R')
-source('5_BaseflowSalinization.R')
+# source('5_BaseflowSalinization.R')
 source('6_DefineCharacteristics.R')
 source('7_Disseminate.R')
 
 select <- dplyr::select # The raster pkg keeps overriding this one so make sure this is correct
 
 c(p1_targets, p2_targets, p3_targets,
-  p4_targets, p5_targets, p6_targets,
-  p7_targets)
+  p4_targets, p6_targets)
